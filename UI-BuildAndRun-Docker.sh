f40541f2ced691872eec4b783cef44c443ad1d91
willGraham01 <1willgraham@gmail.com>
2022-09-20 15:10:33 +0100

    Update Dockerfile so UI can run

--- UI-BuildAndRun-Docker.sh
+++ UI-BuildAndRun-Docker.sh
@@ -3,4 +3,9 @@
 # Ensure that you have run $ xhost +local:docker
 # at least once on your machine prior to attempting to run the GUI in the docker container
 docker image build -t willgraham01/tissueorigami .
-docker container run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix willgraham01/tissueorigami ./UserInterface/TissueFoldingUI
+
+# just try running the UI
+docker container run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix willgraham01/tissueorigami /bin/sh -c "/UserInterface/TissueFoldingUI"
+
+# try and run a simulation we copied across
+#docker container run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix willgraham01/tissueorigami /bin/sh -c "cd /UserInterface/sample07007; /UserInterface/TissueFoldingUI -i /UserInterface/sample07007/modelinput07007 -mode DisplaySave -od /UserInterface/sample07007 -dInput /UserInterface/sample07007/expected_out"
